8985d0642dc180af4ac057c1b87b4af8dbbe40ca
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-12-30 18:46:26 +0530

    placeholder navbar,footer add

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/navbar/Navbar";
+import Footer from "../components/shared/footer.jsx/footer";
 
 const Main = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      {/* navbar */}
+      <Navbar></Navbar>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      {/* footer */}
+      <Footer></Footer>
     </div>
   );
 };
